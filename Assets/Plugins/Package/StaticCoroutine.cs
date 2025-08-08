865f29ca6cf896fffcb4ee94404f356500359dad
Derrick Barra <gambitgamesllc@gmail.com>
2025-08-08 14:54:39 -0400

    Added Stop() function

--- Assets/Plugins/Package/StaticCoroutine.cs
+++ Assets/Plugins/Package/StaticCoroutine.cs
@@ -7,16 +7,33 @@
  * Call StaticCoroutine.Start( YourRoutine() );
  * 
  ********************************************************************/
+#region IMPORTS
+
 using System.Collections;
 using UnityEngine;
+
+#endregion
 
 namespace gambit.staticcoroutine
 {
     public class StaticCoroutine
     {
+        #region PRIVATE - VARIABLES
+
         private static StaticCoroutineRunner runner;
 
+        #endregion
+
+        #region PUBLIC - START
+
+        /// <summary>
+        /// Starts a coroutine
+        /// </summary>
+        /// <param name="coroutine"></param>
+        /// <returns></returns>
+        //---------------------------------------------------------//
         public static Coroutine Start(IEnumerator coroutine)
+        //---------------------------------------------------------//
         {
             if (coroutine == null)
             {
@@ -26,21 +43,66 @@ namespace gambit.staticcoroutine
 
             EnsureRunner();
             return runner.StartCoroutine(coroutine);
-        }
 
+        } //END Start Method
+
+        #endregion
+
+        #region PUBLIC - STOP
+
+        /// <summary>
+        /// Stops a coroutine
+        /// </summary>
+        /// <param name="coroutine"></param>
+        /// <returns></returns>
+        //---------------------------------------------------------//
+        public static void Stop( Coroutine coroutine )
+        //---------------------------------------------------------//
+        {
+            if(coroutine == null)
+            {
+                Debug.LogError( "StaticCoroutine.cs Stop() ERROR: coroutine sent in is null!" );
+                return;
+            }
+
+            EnsureRunner();
+            runner.StopCoroutine(coroutine);
+
+        } //END Stop Method
+
+
+        #endregion
+
+        #region PRIVATE - ENSURE RUNNER EXISTS
+
+        /// <summary>
+        /// Make sure the gameobject running the coroutines exists
+        /// </summary>
+        //------------------------------------------//
         private static void EnsureRunner()
+        //------------------------------------------//
         {
             if (runner == null)
             {
                 runner = new GameObject("[Static Coroutine Runner]").AddComponent<StaticCoroutineRunner>();
                 Object.DontDestroyOnLoad(runner.gameObject);
             }
-        }
 
+        } //END EnsureRunner
+
+        #endregion
+
+        #region PRIVATE - STATIC COROUTINE RUNNER - CLASS
+
+        /// <summary>
+        /// Empty class used as a component reference
+        /// </summary>
         private class StaticCoroutineRunner : MonoBehaviour
         {
         }
 
-    } //END class
+        #endregion
 
-} //END namespace
+    } //END StaticCoroutine Class
+
+} //END gambit.staticcoroutine namespace
